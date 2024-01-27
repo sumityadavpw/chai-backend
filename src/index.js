d4e4873de06447c943ef502181f2d9c51a432c85
sumityadavpw <sumityadav9521@gmail.com>
2024-01-28 01:52:27 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-
+import {app} from './app.js'
 dotenv.config({
-    path: './env'
+    path: './.env'
 })
 
 
@@ -11,7 +11,7 @@ dotenv.config({
 connectDB()
 .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`Server is running at port : ${process.env.PORT}`);
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     })
 })
 .catch((err) => {
@@ -25,24 +25,27 @@ connectDB()
 
 
 
+
+
 /*
 import express from "express"
 const app = express()
-
 ( async () => {
     try {
         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("error", (error) => {
-            console.log("ERR: ", error);
+        app.on("errror", (error) => {
+            console.log("ERRR: ", error);
             throw error
         })
 
         app.listen(process.env.PORT, () => {
-            console.log(`App is listening on port $ {process.env.PORT}`);
+            console.log(`App is listening on port ${process.env.PORT}`);
         })
 
     } catch (error) {
-        console.log("ERROR: ", error)
+        console.error("ERROR: ", error)
         throw err
     }
-})() */
+})()
+
+*/
